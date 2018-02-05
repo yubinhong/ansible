03886795d9aa557554c01569cc79057f679ee49b
chenjianbin <546391242@qq.com>
2018-02-05 16:16:24 +0800

    【优化】显示cdn更新ssl证书失败原因

--- playbooks/get_sslinfo/roles/get_sslinfo/templates/get_sslinfo.py
+++ playbooks/get_sslinfo/roles/get_sslinfo/templates/get_sslinfo.py
@@ -5,9 +5,12 @@ import os
 import time
 import json
 import requests
+import hashlib
+import commands
 IP="{{ inventory_hostname }}"
 BASE_DIR="/etc/letsencrypt/live"
 API="https://oapi.dingtalk.com/robot/send?access_token=07133fed0a8d1c70c34c279653967514ced6f3b3de194b5824250218f9034cef"
+CMDB_API="http://miguanvpn2.f3322.net:23336/api/cdn/update_https/"
 def main():
     domain_list=os.listdir(BASE_DIR)
     text=""
@@ -32,13 +35,45 @@ def main():
             text += "## 到期时间\n\n%s \n\n" % time.strftime("%Y-%m-%d", time.localtime(expire_time))
         elif free_time > 60*60*24*89 and free_time < 60*60*24*90:
             text += "## 服务器\n\n>%s \n\n" % IP
-            text += "## 提示\n\n>%s SSL证书已更新！如有加CDN，请即时更新CDN证书\n\n " % domain
-            text += "## 到期时间\n\n%s \n\n" % time.strftime("%Y-%m-%d", time.localtime(expire_time))
+            text += "## 提示\n\n>%s SSL证书已更新！\n\n " % domain
+            text += "## 注意\n\n>%s \n\n" % update_https(domain)
+            text += "## 到期时间\n\n>%s \n\n" % time.strftime("%Y-%m-%d", time.localtime(expire_time))
     if text!="":
         result=send_to_dingding(text)
         return result
 
 
+
+def update_https(domain):
+    cmd="grep -A 10 'webroot_map' /etc/letsencrypt/renewal/%s.conf |grep %s|awk '{print $1}'" % (domain,domain)
+    result=commands.getoutput(cmd)
+    subdomain_list=result.split('\n')
+    text=""
+    for subdomain in subdomain_list:
+        current_time=int(time.time())
+        m=hashlib.md5()
+        m.update((str(current_time)+'miguan').encode('utf-8'))
+        token=m.hexdigest()
+        payload={}
+        payload['timestamp']=int(time.time())
+        payload['token']=token
+        payload['domain_name']=subdomain
+        payload['ip']=IP
+        f=open('%s/%s/fullchain.pem' % (BASE_DIR,domain),'r')
+        certificate=f.read()
+        f.close()
+        f=open('%s/%s/privkey.pem' % (BASE_DIR,domain),'r')
+        privatekey=f.read()
+        f.close()
+        payload['certificate']=certificate
+        payload['privatekey']=privatekey
+        req=requests.post(CMDB_API,data=payload)
+        req=req.json()
+        if len(req.keys())==1:
+            text +="%s 更新cdn证书成功！" % subdomain
+        else:
+            text +=req['message']
+    return text
 
 
 def send_to_dingding(text):
